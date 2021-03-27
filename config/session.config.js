f315ad3d6ecc669cb94f0b9a473636660d8f0124
Erwan Vallée <erwanp.vallee@gmail.com>
2021-03-27 11:01:27 +0100

    feat: maj connect-mongo

--- config/session.config.js
+++ config/session.config.js
@@ -1,18 +1,20 @@
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const { clientPromise } = require('../database');
 const { app } = require('../app');
 
-app.use(session({
-  secret: 'cersei',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 14 // session 14 days
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 60 * 60 * 24 * 14
+app.use(
+  session({
+    secret: 'cersei',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 14, // session 14 days
+    },
+    store: MongoStore.create({
+      clientPromise: clientPromise.then((m) => m.connection.getClient()),
+      ttl: 60 * 60 * 24 * 14,
+    }),
   })
-}));
+);
--- database/index.js
+++ database/index.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(
-    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log('connexion ok !');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+exports.clientPromise = mongoose.connect(
+  'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
