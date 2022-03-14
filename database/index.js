4be33264a4b4f32e69ff57523a23481cd1e070e1
dymafr <contact@dyma.fr>
2022-03-14 13:53:13 +0100

    maj

--- database/index.js
+++ database/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 exports.clientPromise = mongoose.connect(
   'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
   {
-    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
