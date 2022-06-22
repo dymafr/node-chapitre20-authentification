d1b6526fc98fcbe7a42da3a0b4f9bd7769c7ba3c
dymafr <contact@dyma.fr>
2022-06-22 14:38:55 +0200

    maj passport

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,40 +2,45 @@ const passport = require('passport');
 
 exports.sessionNew = (req, res, next) => {
   res.render('signin', { error: null });
-}
+};
 
 exports.sessionCreate = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
       next(e);
     } else if (!user) {
-      res.render('signin', { error: info.message })
+      res.render('signin', { error: info.message });
     } else {
       req.login(user, (err) => {
-        if (err) { 
-          next(e) 
+        if (err) {
+          next(e);
         } else {
           res.redirect('/');
         }
-      })
+      });
     }
-  })(req, res , next);
-}
+  })(req, res, next);
+};
 
 exports.googleAuth = (req, res, next) => {
   passport.authenticate('google', {
-    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+    scope:
+      'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
   })(req, res, next);
-}
+};
 
 exports.googleAuthCb = (req, res, next) => {
   passport.authenticate('google', {
     successRedirect: '/protected',
-    failureRedirect: '/'
+    failureRedirect: '/',
   })(req, res, next);
-}
+};
 
 exports.sessionDelete = (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-}
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+};
